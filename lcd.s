34f7d6699a693e814ebffb20716b60d0ddc8c623
Carlos Valadão <61719667+arlosValadao@users.noreply.github.com>
2023-04-02 17:27:48 -0300

    Comentários e Melhorias

--- lcd.s
+++ lcd.s
@@ -1,3 +1,9 @@
+/*
+======================================================
+        Altera o modo dos pinos conectados
+		no LCD em modo de saida
+======================================================
+*/
 .macro setLCDPinsOut
 	GPIOPinOut E
 	GPIOPinOut RS
@@ -5,149 +11,335 @@
 	GPIOPinOut d6
 	GPIOPinOut d5
 	GPIOPinOut d4
-	.ltorg
 .endm
 
-
+/*
+======================================================
+        Da um pulso no pino conectado ao enable (E)
+		do display LCD
+======================================================
+*/
 .macro enable
     GPIOPinLow E
-    nanoSleep timeZero, timeMilli15
+    nanoSleep timeZero, time1ms
     GPIOPinHigh E
-    nanoSleep timeZero, timeMilli15
+    nanoSleep timeZero, time1ms
     GPIOPinLow E
 .endm
 
-@ pl = pin low
-@ ph = pin high
-.macro send4bCommand pl0 pl1 pl2 pl3 ph0 ph1
+
+/*
+======================================================
+        Realiza a inicialização do display LCD
+		(com base nas instrucoes do datasheet dele)
+======================================================
+*/
+.macro init
     GPIOPinLow RS
 
-    GPIOPinLow =\pl0
-    GPIOPinLow =\pl1
-    GPIOPinLow =\pl2
-    GPIOPinLow =\pl3
-
-    GPIOPinHigh =\ph0
-    GPIOPinHigh =\ph1
-
+    GPIOPinLow d7
+    GPIOPinLow d6
+    GPIOPinHigh d5
+    GPIOPinHigh d4
     enable
-    @.ltorg
-.endm
+    nanoSleep timeZero, time5ms
 
+    GPIOPinLow d7
+    GPIOPinLow d6
+    GPIOPinHigh d5
+    GPIOPinHigh d4
+    enable    
+    nanoSleep timeZero, time150us
 
-.macro pinsOff p0 p1 p2 p3
-    GPIOPinLow RS
-
-    GPIOPinLow =\p0
-    GPIOPinLow =\p1
-    GPIOPinLow =\p2
-    GPIOPinLow =\p3
+    GPIOPinLow d7
+    GPIOPinLow d6
+    GPIOPinHigh d5
+    GPIOPinHigh d4
     enable
-.endm
 
-
-.macro pinsOn p0 p1 p2 p3
-    GPIOPinLow RS
-
-    GPIOPinHigh =\p0
-    GPIOPinHigh =\p1
-    GPIOPinHigh =\p2
-    GPIOPinHigh =\p3
+    GPIOPinLow d7
+    GPIOPinLow d6
+    GPIOPinHigh d5
+    GPIOPinLow d4
     enable
-.endm
 
+    GPIOPinLow d7
+    GPIOPinLow d6
+    GPIOPinHigh d5
+    GPIOPinLow d4
+    enable
 
-
-@ TInitLCD = Tricky Init LCD
-.macro TInitLCD
-    @function set
-    send4bCommand d7, d6, NULL, NULL, d5, d4
-    nanoSleep timeZero, timeSpecNano5
-
-    @function set
-    send4bCommand d7, d6, NULL, NULL, d5, d4
-    nanoSleep timeZero, timeSpecNano120
-
-    @function set
-    send4bCommand d7, d6, NULL, NULL, d5, d4
-    send4bCommand d7, d6, d4, NULL, d5, NULL
-    nanoSleep timeZero, timeSpecNano120
-    .ltorg
-
-    @function set
-    send4bCommand d7, d6, d4, NULL, d5, NULL
-    pinsOff d7, d6, d5, d4
-    nanoSleep timeZero, timeSpecNano120
-
-    @display off
-    pinsOff d7, d6, d5, d4
-    send4bCommand d6, d5, d4, NULL, d7, NULL
-    nanoSleep timeZero, timeSpecNano120
-
-    @display clear
-    pinsOff d7, d6, d5, d4
-    send4bCommand d7, d6, d5, NULL, d4, NULL
-    nanoSleep timeZero, timeSpecNano120
-
-    @ entry mode set
-    pinsOff d7, d6, d5, d4
-    send4bCommand d7, d4, NULL, NULL, d6, d5
-    nanoSleep timeZero, timeSpecNano120
-    .ltorg
-.endm
-
-
-.macro clearDisplay
     GPIOPinLow d7
     GPIOPinLow d6
     GPIOPinLow d5
     GPIOPinLow d4
     enable
+
+    enable
+
+    GPIOPinHigh d7
+    GPIOPinLow d6
+    GPIOPinLow d5
+    GPIOPinLow d4
+    enable
+
+    GPIOPinLow d7
+    enable
+
+    GPIOPinHigh d4
+    enable
+
+    GPIOPinLow d4
+    enable
+
+    GPIOPinLow d7
+    GPIOPinHigh d6
+    GPIOPinHigh d5
+    enable
+
+    GPIOPinLow d6
+    GPIOPinLow d5
+    enable
+
+    GPIOPinHigh d7
+    GPIOPinHigh d6
+    GPIOPinHigh d5
+    enable
+
+    GPIOPinLow d7
+    GPIOPinLow d6
+    GPIOPinLow d5
+    GPIOPinLow d4
+    enable
+
+    GPIOPinLow d7
+    GPIOPinHigh d6
+    GPIOPinHigh d5
+    GPIOPinLow d4
+    enable
+	.ltorg
+.endm
+
+/*
+======================================================
+		Executa a instrucao de clear do LCD
+======================================================
+*/
+.macro clearDisplay
+	GPIOPinLow RS
+
+	GPIOPinLow d7
+	GPIOPinLow d6
+	GPIOPinLow d5
+	GPIOPinLow d4
+	enable
+	
+	GPIOPinHigh d4
+	enable
+.endm
+
+/*
+======================================================
+	Manda o upper bits da coluna da matriz da tabela
+	de dados do LCD para o LCD
+	(Usada como auxiliar em WriteLCD)
+======================================================
+*/
+.macro prefixNumberDisplay
+    GPIOPinLow d7
+    GPIOPinLow d6
+    GPIOPinHigh d5
+    GPIOPinHigh d4
+    enable
+.endm
+
+/*
+======================================================
+	Escreve um numero no display LCD
+======================================================
+*/
+.macro WriteLCD value
+
+	GPIOPinHigh RS
+	prefixNumberDisplay
+
+	@ D4	
+    mov r1, #0b00001      
+    and r1, \value          @0001 & 0011 -> 0001
+    LDR R0, =d4
+	LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    @ D5
+    mov r1, #0b00010   
+    and r1, \value          @ 0010 & 0011 -> 0010
+    lsr r1, #1              @ Desloca o bit 1x para direita  -> 0001
+    LDR R0, =d5
+	LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    @ D6
+    mov r1, #0b00100      
+    and r1, \value          @ 0100 & 0101 -> 0100
+    lsr r1, #2              @ Desloca o bit 2x para direita  -> 0001
+    LDR R0, =d6
+	LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    @ D7
+    mov r1, #0b01000      
+    and r1, \value          @ 01000 & 01000 -> 01000
+    lsr r1, #3              @ Desloca o bit 3x para direita  -> 00001
+    LDR R0, =d7
+	LDR R7, [R0, #8]
+    BL GPIOPinTurn    
+  	enable
+.endm
+
+
+/*
+======================================================
+	Liga o display LCD, exibe o cursor e o torna
+	piscante
+======================================================
+*/
+.macro displayOn
+	GPIOPinLow RS
+	
+	GPIOPinLow d7	
+	GPIOPinLow d5	
+	GPIOPinLow d6	
+	GPIOPinLow d4
+	enable
+
+	GPIOPinHigh d7
+	GPIOPinHigh d6
+	GPIOPinHigh d5
+	GPIOPinHigh d4
+	enable
+.endm
+
+
+/*
+======================================================
+	Desliga o display LCD
+======================================================
+*/
+.macro displayOff
+	GPIOPinLow RS
+	
+	GPIOPinLow d7	
+	GPIOPinLow d5	
+	GPIOPinLow d6	
+	GPIOPinLow d4
+	enable
+
+	GPIOPinHigh d7
+	enable
+.endm
+
+
+
+/*
+======================================================
+	Desloca o cursor do display LCD para a direita
+======================================================
+*/
+.macro cursorShiftRight
+    GPIOPinLow RS
+
     GPIOPinLow d7
     GPIOPinLow d6
     GPIOPinLow d5
     GPIOPinHigh d4
     enable
-    nanoSleep timeZero, timeSpecMilli155
-.endm
 
-
-.macro ReturnHome
-    pinsOff d7, d6, d5, d4
-    send4bCommand d7, d6, NULL, NULL, d5, d4
-    nanoSleep timeZero, timeSpecMili155
-.endm
-
-
-.macro controlDisplay
-    pinsOff d7, d6, d5, d4
-    pinsOn d7, d6, d5, d4
-    nanoSleep timeZero, timeSpecNano120
-.endm
-
-
-.macro rightCursorShift
-    send4bCommand d7, d6, d5, NULL, d4, NULL
-    send4bCommand d7, d5, d4, NULL, d6, NULL
-    nanoSleep timeZero, timeSpecNano120
-.endm
-
-@ 0100
-@ 1000
-.macro escreveH
-   send4bData d7, d6, NULL, NULL, d5, d4
-   send4bData d7, d4, NULL, NULL, d6, d5 
-.endm
-
-.macro send4bData pl0 pl1 pl2 pl3 ph0 ph1
-    GPIOPinHigh RS
-    GPIOPinLow =\pl0
-    GPIOPinLow =\pl1
-    GPIOPinLow =\pl2
-    GPIOPinLow =\pl3
-
-    GPIOPiHigh =\ph0
-    GPIOPinHigh =\ph1
+    GPIOPinHigh d6
     enable
-    .ltorg
+.endm
+
+
+/*
+======================================================
+	Executa a instrucao Return Home no display
+	LCD
+======================================================
+*/
+.macro returnHome
+	GPIOPinLow RS
+
+	GPIOPinLow d7
+	GPIOPinLow d6
+	GPIOPinLow d5
+	GPIOPinLow d4
+	enable
+
+	GPIOPinHigh d5
+	enable
+.endm
+
+
+/*
+======================================================
+	Escreve um caractere no display LCD
+
+    O Hexadecimal, de acordo com a table do LCD
+    deve vir do seguinte formato |0x upper lower|
+======================================================
+*/
+.macro WriteCharLCD hex
+    MOV R9, \hex
+    GPIOPinHigh RS
+
+    MOV R2, #7
+    BL getBitState
+    LDR R0, =d7
+    LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    MOV R2, #6
+    BL getBitState
+    LDR R0, =d6
+    LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    MOV R2, #5
+    BL getBitState
+    LDR R0, =d5
+    LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    MOV R2, #4
+    BL getBitState
+    LDR R0, =d4
+    LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    enable
+
+    MOV R2, #3
+    BL getBitState
+    LDR R0, =d7
+    LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    MOV R2, #2
+    BL getBitState
+    LDR R0, =d6
+    LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    MOV R2, #1
+    BL getBitState
+    LDR R0, =d5
+    LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    MOV R2, #0
+    BL getBitState
+    LDR R0, =d4
+    LDR R7, [R0, #8]
+    BL GPIOPinTurn
+
+    enable
 .endm
